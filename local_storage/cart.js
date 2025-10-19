74aa66d79b4d27976fe5a2bf221376837cb53c72
md-alif007 <mohammadalif043@gmail.com>
2025-10-19 12:54:29 +0600

    first commit

---
+++ local_storage/cart.js
@@ -0,0 +1,42 @@
+const handleAddToCart = () => {
+
+    const productName = document.getElementById("product-name");
+    const productQuantity = document.getElementById("product-quantity");
+
+    const name = productName.value;
+    const quantity = productQuantity.value;
+
+    saveProductToLocalStorage(name, quantity);
+    displayProduct(name, quantity);
+
+    productName.value = "";
+    productQuantity.value = "";
+}
+
+const displayProduct = (name, quantity) => {
+
+    const productContainer = document.getElementById("product-container");
+
+    const li = document.createElement("li");
+    li.innerText =
+        `
+    ${name} : ${quantity};
+    `
+    productContainer.appendChild(li);
+}
+
+const getProductFromLocalStorage = () => {
+    let cart = {};
+
+    const getProduct = localStorage.getItem("cart");
+
+    if (getProduct) {
+        cart = JSON.parse(getProduct);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (name, quantity) => {
+    const cart = getProductFromLocalStorage();
+    console.log(cart);
+}
