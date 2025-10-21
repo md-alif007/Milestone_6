6fd12f554946111a192a1d228838983a4866f1a7
md-alif007 <mohammadalif043@gmail.com>
2025-10-21 12:44:21 +0600

    recap

---
+++ Js_for_react/app.js
@@ -0,0 +1,69 @@
+// let
+// if elsee
+// const
+
+const friends = ["hero", "srk", "sk", "ak"];
+
+// console.log(friends.length);
+// console.log(friends[0]);
+// friends.push("alif");
+// friends.pop();
+
+// added in firsst
+// friends.unshift("bd");
+
+// deleted from last
+// friends.shift();
+
+// slice (started from 2 and ended at 3 {3 will not be taken})
+// console.log(friends.slice(2,3));
+
+// splice (2 and 3 both will be taken)
+// console.log(friends.splice(2,3));
+
+// console.log(friends);
+
+// ---------------------------------------------
+
+// foor loop
+
+// for (let index = 0; index < friends.length; index++) {
+//     const element = friends[index];
+//     console.log(element);
+// }
+
+// -----------------------------------------------
+
+// function
+
+// function sum(a = 5, b = 3) {
+//     const res = a + b;
+//     console.log(res);
+// }
+// sum()
+
+// function sum(a, b) {
+//     const res = a + b;
+//     return res;
+// }
+// const output = sum(4, 4);
+// console.log(output);
+
+// ------------------------------------
+
+// obj
+const person = {
+    name: "alif",
+    age: 19,
+    friends: ["hero", "srk", "sk", "ak"]
+}
+// console.log(person);
+// console.log(person.name);
+// console.log(person.friends[0]);
+
+// person.name = "md. alif"
+// console.log(person);
+
+
+
+
