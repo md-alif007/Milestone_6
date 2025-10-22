c9d9b66fe06a584c80b4637d5fefc1c95b3e9dca
md-alif007 <mohammadalif043@gmail.com>
2025-10-22 11:35:38 +0600

    array methods

---
+++ Js_for_react/arrayMethods.js
@@ -0,0 +1,23 @@
+const products =
+    [
+        { category: "phone1", brand: "shaomi", color: "red", price: 3000 },
+
+        { category: "phone2", brand: "apple", color: "green", price: 4000 },
+
+        { category: "phone3", brand: "samsung1", color: "blue", price: 2000 },
+
+        { category: "phone3", brand: "samsung2", color: "black", price: 2000 },
+
+        { category: "phone4", brand: "tecno", color: "white", price: 1000 }
+    ]
+
+const result = products.map(product => product.price);
+console.log(result);
+
+products.forEach(product => console.log(product.price));
+
+const appleProduct = products.filter(product => product.brand === "apple");
+console.log(appleProduct);
+
+const particularLProduct = products.find(product => product.category === "phone3");
+console.log(particularLProduct);
