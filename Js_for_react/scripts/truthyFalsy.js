39a96fd8e1a37736cb342fe0866dbb4812ed709f
md-alif007 <mohammadalif043@gmail.com>
2025-10-27 11:34:58 +0600

    truthy and falsy value

---
+++ Js_for_react/scripts/truthyFalsy.js
@@ -0,0 +1,19 @@
+const test = ""
+
+
+// if (test) {
+//     console.log("its a truthy value");
+// } else {
+//     console.log("its a falsy value");
+// }
+
+// test ? console.log("its a truthy value") : console.log("its a falsy value");
+
+// isActive = false;
+
+// const showUser = () => console.log("user active");
+// const hideUser = () => console.log("user inactive");
+
+// isActive ? showUser() : hideUser();
+
+// isActive && showUser();
